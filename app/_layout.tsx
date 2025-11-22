118fd0e403540a010b13e40ca72c9ec5534509d0
dinakar17 <dinakarchennupati777@gmail.com>
2025-11-22 10:42:41 +0530

    feat: add sentry (this is the differentiator)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,44 @@
+import { Button } from "@/components/Button";
 import "../global.css";
 
+import {
+  captureException,
+  feedbackIntegration,
+  init,
+  mobileReplayIntegration,
+  wrap,
+} from "@sentry/react-native";
 import { Stack } from "expo-router";
 
-export default function Layout() {
-  return <Stack />;
-}
+init({
+  dsn: "https://60e054e9764332a263aa51e16758900a@o4504036848173056.ingest.us.sentry.io/4510406722387968",
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [mobileReplayIntegration(), feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default wrap(function Layout() {
+  return (
+    <>
+      <Stack />
+      <Button
+        onPress={() => {
+          captureException(new Error("First error"));
+        }}
+        title="Try!"
+      />
+    </>
+  );
+});
